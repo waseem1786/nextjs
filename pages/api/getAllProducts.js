4b75534d35cb2a788916f64a383f9def305916ee
Waseem <wasimfroz565@gmail.com>
2024-08-10 14:02:28 +0400

    added get all products api

---
+++ pages/api/getAllProducts.js
@@ -0,0 +1,31 @@
+import { MongoClient } from "mongodb";
+
+export default async function (req, res) {
+    if (req.method === "POST") {
+        
+        console.log(process.env.MONGODB_URI)
+        const client = new MongoClient(process.env.MONGODB_URI);
+
+        try {
+            await client.connect();
+
+            console.log('connected')
+
+            // Choose a name for your database
+            const database = client.db("e_store");
+
+            // Choose a name for your collection
+            const collection = database.collection("inventory");
+            const allData = await collection.find({}).toArray();
+
+            res.status(200).json(allData);
+        } catch (error) {
+            console.log('error',error)
+            res.status(500).json({ message: "Something went wrong!" });
+        } finally {
+            await client.close();
+        }
+    } else {
+        res.status(405).json({ message: "Method not allowed!" });
+    }
+}
